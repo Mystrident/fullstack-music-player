39ea930fb8ce3112675ec1f0a8982a60cd9c7e73
pranav <mystrident123@gmail.com>
2026-03-09 00:13:02 +0530

    fix reset password link to use stable frontend domain

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -173,7 +173,7 @@ By storing only the hashed version, the attacker sees useless scrambled data.*/
 
     await user.save();
 
-    const resetUrl = `${process.env.FRONTEND_URL}/reset-password/${resetToken}`;
+    const resetUrl = `${process.env.FRONTEND_URL}/reset-password/${token}`;
 
     await sendMail({
       to: user.emailID,
